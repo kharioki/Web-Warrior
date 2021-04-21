fdcc7cb9deca8f7c6d0ff27e9a2f3ea6064b3bc8
kharioki <antolisah@gmail.com>
2021-04-21 04:27:01 +0300

    styling components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import React from "react"
+import { Link } from 'gatsby'
 import Layout from "../components/Layout"
+import * as styles from '../styles/home.module.css'
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
-          <p>UX designer, mobile & web developer based in Westlands, Kenya</p>
+          <p>UX designer, Mobile & Web developer based in Westlands, Kenya.</p>
+          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
       </section>
     </Layout>
