1235c0759998bd8353af43c8aba8e4d231f04afc
kharioki <antolisah@gmail.com>
2021-04-22 04:00:58 +0300

    static files

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ export default function Home() {
           <p>UX designer, Mobile & Web developer based in Westlands, Kenya.</p>
           <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
+        <img src="/banner.png" alt="site-banner" style={{ maxWidth: '100%' }} />
       </section>
     </Layout>
   )
