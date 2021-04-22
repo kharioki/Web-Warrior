0832b100b75869a54f5fef70759daddfbdd5b2f5
kharioki <antolisah@gmail.com>
2021-04-22 12:06:49 +0300

    generating pages

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -39,6 +39,7 @@ export const query = graphql`
             frontmatter {
                 title
                 stack
+                slug
                 thumb {
                     childImageSharp {
                         fluid {
